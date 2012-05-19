39c9c7edc8780018a0e730567d80faa9c021f9c6
Michał Czyżewski <michal@mczyzewski.com>
2012-05-19 17:16:05 +0200

    JavaScript makes so much more sense

--- static/js/forms.js
+++ static/js/forms.js
@@ -1,89 +1,140 @@
 var scandal_types = [];
+var event_types = [];
+var scandal_id = null;
 
-function refresh_subtypes(scandal_type) {
-    if(scandal_type == undefined) {
-        scandal_type = $("#scandal_type").val();
-    }
-    $("#scandal_subtype").attr("disabled", "disabled").html('<option value="0">(brak)</option>');
-    $.getJSON("/options/scandal_subtypes", {"parent": scandal_type}, function(data) {
+// HACK for subtype loading
+var subtype_id = null;
+// end HACK
+
+/* mustache templates */
+
+var tpl_select_option = '<option value="{{id}}">{{name}}</option>';
+var tpl_consequence = '<input type="checkbox" id="scandal_consequence_{{id}}"><label for="scandal_consequence_{{id}}">{{name}}</label>';
+// FIXME: this is nasty
+var tpl_event_form = '<li>'
+    + '<label for="event-1-location">Lokacja</label> <select name="event-1-location">'
+    + '<option value="2">ogólnopolska</option>'
+    + '</select>'
+    + '<br>Czas'
+    + '<br><label for="event-1-type">Typ</label> <select name="event-1-type">'
+    + '<option value="2">postępowanie sądowe</option>'
+    + '</select>'
+    + '<br><label for="event-1-description">Opis</label>'
+    + '<br><textarea id="event-1-description"></textarea>'
+    + '</li>';
+
+/* helper functions */
+
+function load_scandal_types( callback ) {
+    $.getJSON("/options/scandal_types", function(data) {
+        /* add new options to the list */
         $.each(data, function(index, value) {
-            $("#scandal_subtype").append('<option value="'+value.id+'">'+value.name+'</option>');
+            $("#scandal_type").append(Mustache.render(tpl_select_option, value));
         });
-        $("#scandal_subtype").removeAttr("disabled");
+
+        /* set handler to refresh subtypes */
+        $("#scandal_type").change(function() {
+            /* disable and clear subtype list */
+            $("#scandal_subtype").attr("disabled", "disabled").html('<option value="0">(brak)</option>');
+
+            /* determine current scandal type */
+            var type_id = $("#scandal_type").val();
+            $.getJSON("/options/scandal_subtypes", {"parent": type_id}, function(data) {
+                $.each(data, function(index, value) {
+                    $("#scandal_subtype").append(Mustache.render(tpl_select_option, value));
+                });
+                /* enable subtype list */
+                $("#scandal_subtype").removeAttr("disabled");
+
+                // HACK for subtype loading
+                $("#scandal_subtype").val(subtype_id);
+                // end HACK
+            });
+        });
+
+        callback();
+    });
+}
+
+function load_scandal_consequences( callback ) {
+    $.getJSON("/options/scandal_consequences", function(data) {
+        $.each(data, function(index, value) {
+            $("#scandal_consequences").append(Mustache.render(tpl_consequence, value));
+        });
+
+        callback();
     });
 }
 
 function add_event_form(event_dict) {
-    var template = '<li>'
-        + '<label for="event-1-location">Lokacja</label> <select name="event-1-location">'
-        + '<option value="2">ogólnopolska</option>'
-        + '</select>'
-        + '<br>Czas'
-        + '<br><label for="event-1-type">Typ</label> <select name="event-1-type">'
-        + '<option value="2">postępowanie sądowe</option>'
-        + '</select>'
-        + '<br><label for="event-1-description">Opis</label>'
-        + '<br><textarea id="event-1-description"></textarea>'
-        + '</li>';
-    $("#btn-add-event").before(Mustache.render(template, event_dict));
+    /* default value for optional argument */
+    if(typeof(event_dict) === "undefined") {
+        event_dict = {};
+    }
+
+    /* add new event form to the bottom, just before the button */
+    $("#btn-add-event").before(Mustache.render(tpl_event_form, event_dict));
 }
 
 $(document).ready(function() {
 
-    // overload form events and buttons
+    load_scandal_types(function() {
+        load_scandal_consequences(function() {
+            /* which scandal are we talking about, again? */
+            var path = window.location.pathname.split("/");
+            var scandal_str = parseInt(path[path.length-1]);
+            scandal_id = isNaN(scandal_str) ? null : scandal_str;
 
-    $("#btn-add-event a").attr('href', 'javascript:add_event_form({})');
+            /* if id was given, load me some scandal data */
+            if(scandal_id !== null) {
+                $.getJSON("/api/scandal/"+scandal_id, function(data) {
+                    $("#name").val(data.name);
+                    $("#description").val(data.description);
+                    $("#scandal_type").val(data.type_id).change();
 
-    // load possible options to lists
+                    // FIXME: do subtype loading properly
+                    // This does not work, as it tries to set subtype
+                    // before list is loaded. List loads in event handler,
+                    // so that we can't specify callback here.
+                    //$("#scandal_subtype").val(data.subtype_id);
 
-    // load scandal data & events data while making sure options on the subtype list load ok
-    $("#form-scandal").submit(function() {
-        var scandal = {};
-        $('input[type="text"]').each(function() {
-            scandal[$(this).attr('name')] = $(this).val();
+                    // HACK for subtype setting
+                    subtype_id = data.subtype_id;
+                    // end HACK
+
+                    // TODO: load me some events
+                });
+            } else alert("new scandal!");
+
+            /* button handlers */
+
+            $("#btn-add-event a").attr('href', 'javascript:add_event_form()');
+
+            /* form handlers */
+
+            $("#form-scandal").submit(function() {
+                var scandal = {};
+                /*
+
+                // save scandal data
+                // iterate through events list and save events with non-empty descriptions
+
+                $('input[type="text"]').each(function() {
+                    scandal[$(this).attr('name')] = $(this).val();
+                });
+                $('input[type="checkbox"]').each(function() {
+                    scandal[$(this).attr('name')] = ( $(this).attr('checked') == "checked" ? true : false );
+                });
+
+                $.post("/scandal/new", {'json': JSON.stringify(scandal)}, function(data) {
+                    $("#dialog-scandal").dialog({ autoOpen: false, title: "Afera dodana" }).html('Przejdź do <a href="/">strony głównej</a>.').dialog("open")
+                });
+                */
+
+                return false;
+            });
+
         });
-        $('input[type="checkbox"]').each(function() {
-            scandal[$(this).attr('name')] = ( $(this).attr('checked') == "checked" ? true : false );
-        });
-
-        $.post("/scandal/new", {'json': JSON.stringify(scandal)}, function(data) {
-            $("#dialog-scandal").dialog({ autoOpen: false, title: "Afera dodana" }).html('Przejdź do <a href="/">strony głównej</a>.').dialog("open")
-        });
-
-        return false;
     });
 
-    /* load option lists and data from the API */
-    $.getJSON("/options/scandal_types", function(data) {
-        $.each(data, function(index, value) {
-            $("#scandal_type").append('<option value="'+value.id+'">'+value.name+'</option>');
-        });
-        $("#scandal_type").change(function() {
-            refresh_subtypes($(this).val());
-        });
-
-        /* determine which scandal are we talking about */
-        var path = window.location.pathname.split("/");
-        var scandal_id = path[path.length-1];
-        $.getJSON("/api/scandal/"+scandal_id, function(data) {
-            $("#name").val(data.name);
-            $("#description").val(data.description);
-            $("#scandal_type").val(data.type_id);
-            refresh_subtypes();
-            // FIXME: incorrect type/subtype loading
-            // get to know it, then try to impress somebody
-            $("#scandal_subtype").val(data.subtype_id);
-
-            // if there are no events
-            // FIXME
-            // load one empty event form
-            add_event_form({});
-        });
-    });
-    
-    $.getJSON("/options/scandal_consequences", function(data) {
-        $.each(data, function(index, value) {
-            $("#consequences").append('<input type="checkbox" id="consequence-'+value.id+'"><label for="consequence-'+value.id+'">'+value.name+'</label>');
-        });
-    });
 });
